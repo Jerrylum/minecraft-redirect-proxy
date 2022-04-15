23462cf47405f3ea550578fb3db64a1832bd66f7
Jerrylum <serverofjerry@gmail.com>
2022-04-15 17:37:39 +0800

    implement new proxy mode

--- redirect-proxy.py
+++ redirect-proxy.py
@@ -1,11 +1,42 @@
+import typing
 from xmlrpc.client import ProtocolError
 
 from quarry.net.protocol import Protocol, protocol_modes_inv
 from quarry.net.server import ServerProtocol, ServerFactory
 from quarry.net.client import ClientFactory
-from twisted.internet import reactor
+from twisted.names import client, dns
+from twisted.internet import reactor, defer
 
 from enum import Enum
+
+
+def connect_addr_to_upstream_addr(connect_addr: str, server_domain: str) -> typing.Tuple[str, int]:
+
+    if not connect_addr.endswith("." + server_domain):
+        raise ValueError(
+            "connect_addr must end with .{}".format(server_domain))
+
+    temp = connect_addr[:-len(server_domain) - 1]
+    ext_idx = temp.rfind(".")
+
+    if ext_idx < 1:
+        raise ValueError("connect_addr must be in the form of <host>.[port].<server_domain>")
+
+    # TODO handle raw IP input
+    # handle localhost
+
+    lookup_host = temp
+    lookup_port = 25565
+    ext = temp[ext_idx + 1:]
+
+    if ext.isdigit():
+        lookup_host = temp[:ext_idx]
+        lookup_port = int(ext)
+
+    if 0 < lookup_port > 65535:
+        raise ValueError("port must be between 0 and 65535")
+
+    return lookup_host, lookup_port
 
 
 class ProxyMode(Enum):
@@ -115,39 +146,92 @@ class MyDownstream(ServerProtocol):
     pass_through_stream = None
     hidden_connect_stream = None
     login_start_buff = None
+    upstream_host: str = None
+    upstream_port: int = None
+    upstream_dns_deffered: defer.Deferred = None
+
+    def dns_result_set_connect_host(self, result):
+        try:
+            rr = result[0][0]
+            if rr.type == dns.SRV:
+                self.upstream_host = str(rr.payload.target)
+                self.upstream_port = rr.payload.port
+
+        except:
+            # XXX: should improve how we handle errback
+            pass
+
+        print('resolved', self.upstream_host, self.upstream_port)
+        self.upstream_dns_deffered = None
+
+    def motd_upstream_connect(self, _=None):
+        motd = MotdSyncFactory()
+        motd.mother_server = self
+        motd.connect(self.upstream_host,
+                     self.upstream_port)
 
     def hidden_upstream_connect(self):
         hidden = HiddenFactory()
         hidden.mother_server = self
-        hidden.connect(self.factory.hidden_connect_host,
-                       self.factory.hidden_connect_port)
+        hidden.connect(self.factory.args.hidden_connect_host,
+                       self.factory.args.hidden_connect_port)
 
-    def pass_through_connect(self):
+    def pass_through_connect(self, _=None):
         pro = PassThroughFactory()
         pro.mother_server = self
-        pro.connect(self.factory.connect_host, self.factory.connect_port)
+        pro.connect(self.upstream_host, self.upstream_port)
+
+    def packet_handshake(self, buff):
+        buff.save()
+
+        self.upstream_host = self.factory.args.pass_through_host
+        self.upstream_port = self.factory.args.pass_through_port
+
+        buff.unpack_varint()
+        connect_host = buff.unpack_string()
+        server_domain = self.factory.args.domain
+
+        buff.restore()
+        super().packet_handshake(buff)
+
+        if self.factory.args.mode != ProxyMode.hidden:
+            if self.factory.args.mode == ProxyMode.pass_through_by_domain:
+                try:
+                    self.upstream_host, self.upstream_port = connect_addr_to_upstream_addr(connect_host, server_domain)
+                except:
+                    self.close()
+                    return
+
+            if self.upstream_port == 25565:
+                self.upstream_dns_deffered = client \
+                    .lookupService('_minecraft._tcp.' + self.upstream_host, [10]) \
+                    .addCallback(self.dns_result_set_connect_host) \
+                    .addErrback(self.dns_result_set_connect_host)
 
     def packet_login_start(self, buff):
         if self.login_expecting != 0:
             raise ProtocolError("Out-of-order login")
 
-        if self.factory.mode == ProxyMode.hidden:
+        if self.factory.args.mode == ProxyMode.hidden:
             self.hidden_upstream_connect()
         else:
-            self.pass_through_connect()
+            if self.upstream_dns_deffered != None:
+                self.upstream_dns_deffered.addCallback(self.pass_through_connect)
+            else:
+                self.pass_through_connect()
 
         self.login_start_buff = buff.read()
 
     def packet_login_encryption_response(self, buff):
         # must be from hidden client
-        self.hidden_connect_stream.send_packet(
-            "login_encryption_response", buff.read())
+        self.hidden_connect_stream.send_packet("login_encryption_response", buff.read())
 
     def packet_status_request(self, buff):
-        if self.factory.sync:
-            ms = MotdSyncFactory()
-            ms.mother_server = self
-            ms.connect(self.factory.connect_host, self.factory.connect_port)
+        if self.factory.args.sync:
+            if self.upstream_dns_deffered != None:
+                self.upstream_dns_deffered.addCallback(self.motd_upstream_connect)
+            else:
+                self.motd_upstream_connect()
         else:
             super().packet_status_request(buff)
 
@@ -169,12 +253,7 @@ class MyDownstream(ServerProtocol):
 
 class MyDownstreamFactory(ServerFactory):
     protocol = MyDownstream
-    connect_host = None
-    connect_port = None
-    hidden_connect_host = None
-    hidden_connect_port = None
-    mode = ProxyMode.pass_through
-    sync = True
+    args = None
     motd = "A Minecraft Server"
 
 
